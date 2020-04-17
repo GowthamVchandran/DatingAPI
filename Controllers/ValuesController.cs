f8df7ac097a6e57c6159dac73e8e4264599f70d2
gowthamc <gowthamchandranv@gmail.com>
2020-04-17 16:23:02 +0530

    Initial Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        [Route("Test")]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.UserValues.ToListAsync();
+
+            return Ok(result);
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        [Route("Testid")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var getDetails = await _context.UserValues.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (getDetails == null)
+                return BadRequest("Record doesn't exists");
+
+            return Ok(getDetails);
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=USER-PC\\SQLEXPRESS;Initial Catalog=student;Integrated Security=True");
+        }
+        public DataContext()
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserValue> UserValues { get; set; }
+    }
+}
